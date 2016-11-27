dc2a14a79342d854e5f1b72a5579cb4062220716
Alexander Babkin <babkin.a@tecomgroup.ru>
2016-11-28 01:51:09 +0300

    UI Views

--- Recaster.Client/MainWindow.xaml.cs
+++ Recaster.Client/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Recaster.Client
         {
             InitializeComponent();
         }
+
+        private void AddMulticastSourceButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
